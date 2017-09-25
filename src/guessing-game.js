862bfa4b0a3e04abcbf3a15d08b41a6488e57815
YuliaPisarevich_github <yulik.997@gmail.com>
2017-09-25 20:50:53 +0300

    1 version

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,19 +1,29 @@
+var min, max, average;
+
 class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
+		this.min=0
+		this.max=0
+		this.average=0
 
     }
 
     guess() {
+		this.min=min
+		this.average= Math.ceil(this.min + (this.max - this.min) / 2)
+		return this.average
 
     }
 
     lower() {
+		this.max=this.average
 
     }
 
     greater() {
+		this.min=this.average
 
     }
 }
